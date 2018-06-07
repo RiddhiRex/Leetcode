c10f00200b9b31fbc20e4be935f82c6b53b754c2
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-06-07 17:26:56 -0400

    Create Find the Difference.py

---
+++ Find the Difference.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        dict ={}
+        for each in s:
+            if each in dict.keys():
+                dict[each]=dict[each]+1
+            else:
+                dict[each]=1
+        for each in t:
+            if each in dict.keys():
+                dict[each]=dict[each]-1
+            else:
+                print each
+                return each
+        for k, v in dict.iteritems():
+            if v!=0:
+                print k
+                return k
