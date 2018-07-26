005b5b2738f72d28fe851f378164914e69f5e136
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-07-26 00:58:10 -0400

    Create Find Bottom Left Tree Value.py

---
+++ Find Bottom Left Tree Value.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        ans = []
+        def traverse(node, depth, l):
+            if node is None:
+                return None
+            if depth>len(ans):
+                ans.append(node.val)
+            traverse(node.left, depth+1, l)
+            traverse(node.right, depth+1, l)
+            
+        if root is None:
+            return None
+        else:
+            traverse(root, 1, [])
+            return ans[-1]
+        
