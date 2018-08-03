d568a9b61cce9d5857d826a03f7b422188f05826
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-08-03 01:56:36 -0400

    Create Binary Tree Pruning.py

---
+++ Binary Tree Pruning.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if(root is None):
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if(root.left is None and root.right is None and root.val==0):
+            return None
+        return root
