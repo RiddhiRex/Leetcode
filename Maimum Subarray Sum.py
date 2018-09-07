eae3806cc994c1fcf44acd46c3ede1c61f2e1675
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-09-06 22:58:09 -0400

    Update Maimum Subarray Sum.py

--- Maimum Subarray Sum.py
+++ Maimum Subarray Sum.py
@@ -4,7 +4,13 @@ class solution:
     max_ends_here=0
     max_so_far=0
     for i in range(len(a)):
+      
         max_ends_here=max_ends_here+a[i]
+         
+        if(max_ends_here<0):
+            max_ends_here=0
+            
         if(max_so_far<max_ends_here):
             max_so_far=max_ends_here
+            
     return max_so_far
