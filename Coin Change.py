5d199f84cf7605f04da1ac3ede53475cebc17d6f
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-09-07 01:44:14 -0400

    Create Coin Change.py

---
+++ Coin Change.py
@@ -0,0 +1,20 @@
+#Logic taken from https://www.youtube.com/watch?v=NJuKJ8sasGk
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        cnt = [float("inf")]*(amount+1)
+        cnt[0]=0
+
+        for coin in coins:
+            for i in range(len(cnt)):
+                if(i>=coin):
+                    cnt[i]=min(cnt[i], cnt[i-coin]+1)
+                    
+        if(cnt[amount]==float("inf")):
+            return -1
+        else:
+            return cnt[amount]
