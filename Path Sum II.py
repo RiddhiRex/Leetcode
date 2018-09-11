e461fe802b8c8f29c7f1ec347e3b871e6930a513
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-09-10 20:54:17 -0400

    Update Path Sum II.py

--- Path Sum II.py
+++ Path Sum II.py
@@ -1,3 +1,5 @@
+#Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+#
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
