68540479432561942f964168bbcf7db2e3803afd
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-09-10 22:44:57 -0400

    Update Coin Change.py

--- Coin Change.py
+++ Coin Change.py
@@ -1,4 +1,9 @@
+#You are given coins of different denominations and a total amount of money amount. 
+#Write a function to compute the fewest number of coins that you need to make up that amount. 
+#If that amount of money cannot be made up by any combination of the coins, return -1.
+
 #Logic taken from https://www.youtube.com/watch?v=NJuKJ8sasGk
+
 class Solution(object):
     def coinChange(self, coins, amount):
         """
