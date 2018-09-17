d1d0da7f29cef6682bdd93e336bab0cd8c4bad90
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-09-17 14:23:40 -0400

    Create Jump Game II.py

---
+++ Jump Game II.py
@@ -0,0 +1,19 @@
+#As per the logic from https://www.youtube.com/watch?v=cETfFsSTGJI
+
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums)<=1):
+            return 0
+        
+        jumps = [float("inf")]*len(nums)
+        jumps[0]=0
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                if nums[j]+j>=i:
+                    jumps[i]=min(jumps[i], jumps[j]+1)
+        return jumps[len(nums)-1]
+                
