d86e4571a11bcab394df9d6a5e9f47d14f17768c
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-10-07 20:18:36 -0400

    Update Longest Substring with At Least K Repeating Characters.py

--- Longest Substring with At Least K Repeating Characters.py
+++ Longest Substring with At Least K Repeating Characters.py
@@ -26,3 +26,35 @@ class Solution:
                 i = j
             return maxlen
         return longstr(s, k, 0, len(s))
+
+#method2:
+import collections
+class Solution(object):
+    def longsubstr(self, s, k, st, ed):
+        maxlen=0
+        dic = collections.Counter(s[st:ed])
+
+        i=st
+        while(i<ed):
+            while(i<ed and dic[s[i]]<k):
+                i+=1
+            j=i
+            
+            while(j<ed and dic[s[j]]>=k):
+                j+=1
+    
+            if i==st and j==ed:
+                return ed-st
+
+            maxlen=max(maxlen, self.longsubstr(s, k, i, j))
+            i=j
+            
+        return maxlen
+        
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        return self.longsubstr(s, k, 0, len(s))
