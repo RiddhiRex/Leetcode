7848d9b050820c61e7c9c8debe33af4bc6f77a26
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-10-13 16:19:43 -0400

    Update Subarray Sum Equals K.py

--- Subarray Sum Equals K.py
+++ Subarray Sum Equals K.py
@@ -17,4 +17,23 @@ class Solution(object):
                     cnt+=1
         return cnt
                 
-            
+  #method 2:
+import collections
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        result=0
+        d=collections.defaultdict(int)
+        d[0]=1
+        total_sum=0
+        for i in range(len(nums)):
+            total_sum+=nums[i]
+            result+=d[total_sum-k]
+            d[total_sum]+=1
+
+        return result  
