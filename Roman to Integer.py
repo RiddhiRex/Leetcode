bb73e606f4b3a9f0789f179eb52b3326588ce6fe
Riddhi Rex <riddhirex.antonyrex@stonybrook.edu>
2018-10-13 20:01:00 -0400

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,34 +1,16 @@
-class Solution:
+class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        dict = {}
-        dict["I"]=1
-        dict["V"]=5
-        dict["X"]=10
-        dict["L"]=50
-        dict["C"]=100
-        dict["D"]=500
-        dict["M"]=1000
-        prev=0
-        cnt =0
-        sum=0
-        wrong =["IIII", "VVVV","XXXX","LLLL","CCCC","DDDD","MMMM","IXI","IVI","IIV","IIX","ILI","ICI","IDI", "IMI"]
-        for each in wrong:
-            if(s.find(each)!=-1):
-                return False
-        for each in s:
-            if each not in dict:
-                print(False)
-            if(dict[each] <= sum and dict[each]<=prev):
-                sum=sum+dict[each]
-            elif(dict[each]>sum):
-                sum = dict[each]-sum
-            elif(dict[each]>prev):
-                sum = sum+dict[each]-prev-prev
-            prev = dict[each]
-
-        return sum                
-            
+        
+        d= {"I": 1, "V": 5, "X": 10, "L": 50, "C":100, "D": 500, "M": 1000}
+        ans=0
+        for i in range(len(s)):
+            if i>0 and d[s[i]]>d[s[i-1]]:
+                ans+= (d[s[i]] - 2*d[s[i-1]])
+            else:
+                ans+=d[s[i]]
+        return ans
+        
