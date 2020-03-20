98bd04f4804c929c3f44747738c18beef1af585d
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-03-19 23:21:09 -0700

    Update Merge Sorted Array.py

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -31,4 +31,31 @@ class Solution(object):
                 break
         return 
             
+        
+        Solution 2:
+   class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        l = m+n-1
+        m-=1
+        n-=1
+        while(m>=0 and n>=0):
+            if nums1[m]>nums2[n]:
+                nums1[l]= nums1[m]
+                m-=1
+            else:
+                nums1[l]= nums2[n]
+                n-=1
+            l-=1
+        if m>=0:
+            nums1[0:l+1]=nums1[0:m+1]
+        if n>=0:
+            nums1[0:l+1]=nums2[0:n+1]
+                
                 
