2ac2673b80065eeae9cbee1231ba463cd286c698
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-03-26 15:39:32 -0700

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -17,3 +17,12 @@ class Solution:
         for each in sorted(dict.items(), key=operator.itemgetter(1), reverse=True)[:k]:
             l.append(each[0])
         return l
+
+    
+    solution 2:
+    class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = collections.Counter(nums)
+        l = sorted(d.items(), key=operator.itemgetter(1), reverse=True)[0:k]
+        return [i for i,j in l]
+        
