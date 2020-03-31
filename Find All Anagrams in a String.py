4c61272ce521bb8c077e2bffc1d1ff91ffdc4ac7
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-03-30 22:38:48 -0700

    Update Find All Anagrams in a String.py

--- Find All Anagrams in a String.py
+++ Find All Anagrams in a String.py
@@ -17,3 +17,14 @@ class Solution(object):
         return ans
             
         
+Solution 2:
+    class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ans = []
+        pp = sorted(p)
+        for i in range(len(s)-len(p)+1):
+            if sorted(s[i:i+len(p)])==pp:
+                ans.append(i)
+        return ans
+            
+        
