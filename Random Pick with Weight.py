83e8bd166a074db8c8038ff43acf2646f5adc85e
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-04-03 18:16:57 -0700

    Create Random Pick with Weight.py

---
+++ Random Pick with Weight.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self, w: List[int]):
+        self.sum = [0]*len(w)
+        for i, no in enumerate(w):
+            if i==0:
+                self.sum[i]=no
+            else:
+                self.sum[i]=self.sum[i-1]+no
+        print(self.sum)
+
+    def pickIndex(self) -> int:
+        val = random.randint(0, self.sum[-1]-1)
+        l=0
+        r = len(self.sum)-1
+        while(l<r):
+            mid=(l+r)//2
+            if self.sum[mid]<=val:
+                l = mid+1
+            else:
+                r = mid
+        return r
