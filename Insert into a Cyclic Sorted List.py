37b7c90ccd2e98b5d2ca5ba9167f095c6397cc53
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-04-04 12:33:52 -0700

    Create Insert into a Cyclic Sorted List.py

---
+++ Insert into a Cyclic Sorted List.py
@@ -0,0 +1,33 @@
+'''
+# Definition for a Node.
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+'''
+class Solution:
+    def insert(self, head, insertVal):
+        """
+        :type head: Node
+        :type insertVal: int
+        :rtype: Node
+        """
+
+        if head==None:
+            head = Node(insertVal, None)
+            head = new_node
+            head.next=head
+            return head
+        prev = head
+        cur = head.next
+        while(cur!=head):
+            if prev.val<=insertVal and cur.val>=insertVal:
+                break
+            if prev.val>cur.val and (prev.val<=insertVal or cur.val>=insertVal):
+                break
+            prev = cur
+            cur=cur.next
+            
+        prev.next = Node(insertVal, cur)
+        return head
+        
