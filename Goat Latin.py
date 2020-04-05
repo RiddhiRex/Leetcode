3064d80446ba1db5a814856764f5326234199609
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-04-04 21:24:10 -0700

    Update Goat Latin.py

--- Goat Latin.py
+++ Goat Latin.py
@@ -1,4 +1,5 @@
-class Solution(object):
+ #Solution 1:
+    class Solution(object):
     def toGoatLatin(self, S):
         """
         :type S: str
@@ -17,5 +18,14 @@ class Solution(object):
             print(wrd)
         return " ".join(l)
             
-        
+#Solution 2:
+class Solution:
+    def toGoatLatin(self, S: str) -> str:
+        vowels=list("aeiouAEIOU")
+        wrds = S.split(" ")
+        for i, w in enumerate(wrds):
+            if w[0] not in vowels:
+                w = w[1:]+w[:1]
+            wrds[i]=w+"ma"+(i+1)*"a"
+        return " ".join(wrds)
         
