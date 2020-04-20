4e6d0d1455b08c3604166e233e9775eddc2c7c3d
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2020-04-20 01:04:25 -0700

    Update Reverse Bits.py

--- Reverse Bits.py
+++ Reverse Bits.py
@@ -1,1 +1,7 @@
-        if len(str(n))
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        b = bin(n)[2:][::-1]
+        padding = 32-len(b)
+        no = b+"0"*padding
+        return int(no, 2)
+        
