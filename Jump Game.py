2821633429f759dde877526ee6207e58cf964599
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-02-29 12:55:14 -0800

    Update Jump Game.py

--- Jump Game.py
+++ Jump Game.py
@@ -1,16 +1,22 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        step = nums[0]
-        if(len(nums)<=1):
-            return True
-        for i in range(1, len(nums)):
-            if step>0:
-                step-=1
-                step=max(step, nums[i])
-            else:
-                return False 
-        return True
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0  # Maximum index that can be reached
+        last_index = len(nums) - 1
+        
+        # Iterate through the list
+        for i in range(len(nums)):
+            # Update the maximum index that can be reached
+            max_reach = max(max_reach, i + nums[i])
+            
+            # If the maximum index that can be reached is beyond or at the last index,
+            # then it's possible to reach the last index
+            if max_reach >= last_index:
+                return True
+        
+            # If the current index cannot be reached (i.e., it's a dead end)
+            # and there is no way to continue further, return False
+            if max_reach <= i:
+                return False
+        
+        # If the loop completes without returning True, it means the last index cannot be reached
+        return False
