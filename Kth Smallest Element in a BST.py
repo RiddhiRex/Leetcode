adecb60d7fee45b4f3582f244dd082d3194c6486
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-02-29 14:47:19 -0800

    Update Kth Smallest Element in a BST.py

--- Kth Smallest Element in a BST.py
+++ Kth Smallest Element in a BST.py
@@ -1,4 +1,32 @@
 # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.i=0
+        self.ans = None
+        def traverse(node):
+            if node is None:
+                return None
+            else:
+                traverse(node.left)
+                self.i+=1
+                if self.i==k:
+                    self.ans = node.val
+                    return
+                traverse(node.right)
+        traverse(root)
+
+        return self.ans
+                
+        
+
+
+solution 2
+# Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
