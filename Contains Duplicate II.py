2dd5c816960b051586e54f0843b2b3126dfe46ae
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-01 14:05:05 -0800

    Update Contains Duplicate II.py

--- Contains Duplicate II.py
+++ Contains Duplicate II.py
@@ -1,3 +1,17 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        d = collections.defaultdict(list)
+        for i, each in enumerate(nums):
+            if each not in d:
+                d[each].append(i)
+            else:
+                for v in d[each]:
+                    if abs(i-v)<=k:
+                        return True
+                d[each].append(i)
+        return False
+        
+        
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
