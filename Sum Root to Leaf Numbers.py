bf36c1004ecd97ee16b886e247c55819c189cbbf
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-03 15:29:13 -0800

    Update Sum Root to Leaf Numbers.py

--- Sum Root to Leaf Numbers.py
+++ Sum Root to Leaf Numbers.py
@@ -1,4 +1,30 @@
 # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def traverse(node, num):
+            if node.left is None and node.right is None:
+                self.total+=int(num)
+            if node.left is not None:
+                traverse(node.left, num+str(node.left.val))
+            if node.right is not None:
+                traverse(node.right, num+str(node.right.val))
+            return
+
+        self.total = 0
+        if root is None:
+            return 0
+        traverse(root, str(root.val))
+        return self.total
+
+
+        
+
+# Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
