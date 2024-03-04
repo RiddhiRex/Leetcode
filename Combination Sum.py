fd4c6e208ffdd451217062bfe4c9ddd0ad57bca6
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-04 10:06:14 -0800

    Update Combination Sum.py

--- Combination Sum.py
+++ Combination Sum.py
@@ -1,5 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.ans = []
+        def calculate(nums, cur_list, total):
+            if total==target:
+                self.ans.append(cur_list)
+            elif total>target:
+                return
+            for i in range(len(nums)):
+                calculate(nums[i:], cur_list+[nums[i]], total+nums[i])
+        calculate(candidates, [], 0)
+        return self.ans
+        
+        
+        
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
         def combinationSumRecurse(candidates, start, path, ans, tmptarget):
             if tmptarget==0:
