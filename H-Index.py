04c8721c23a169fcd6521d72df45da4f15d8fd50
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-22 13:50:18 -0700

    Update H-Index.py

--- H-Index.py
+++ H-Index.py
@@ -1,5 +1,20 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        citations = sorted(citations, reverse=True)
+        h_index = 0
+        for i in range(len(citations)):
+            print(citations[i])
+            if ((i+1)<=citations[i]):
+                h_index=i+1
+            else:
+                break
+        return h_index
+        
+        
+        
+        
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
         n = len(citations)
         print(citations)
