ab78d4ce378685e64f7dff92a82dbbb22fa2cf5d
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-27 11:35:34 -0700

    Update String Compression.py

--- String Compression.py
+++ String Compression.py
@@ -1,25 +1,33 @@
-class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        i=0
-        cnt=1
-        l=len(chars)
-        for j in range(1, len(chars)+1):
-            if j<l and chars[j]==chars[j-1]:
-                cnt+=1
-            else:
-                chars[i]=chars[j-1]
-                i+=1
-                if cnt>1:
-                    for k in str(cnt):
-                        chars[i]=k
-                        i+=1
-                cnt=1
-        return i 
-    
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+
+        if not chars:
+            return 0
+        
+        # Initialize pointers
+        write_idx = 0
+        read_idx = 0
+        
+        while read_idx < len(chars):
+            char = chars[read_idx]
+            count = 0
+            
+            # Count consecutive repetitions of current character
+            while read_idx < len(chars) and chars[read_idx] == char:
+                read_idx += 1
+                count += 1
+            
+            # Write compressed character and count
+            chars[write_idx] = char
+            write_idx += 1
+            if count > 1:
+                for digit in str(count):
+                    chars[write_idx] = digit
+                    write_idx += 1
+        
+        return write_idx
+
+            
  #solution2
 class Solution:
     def compress(self, chars):
