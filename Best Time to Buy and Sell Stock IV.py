4cc2f5253b5c3378a9f9e1db677faac131815d25
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-27 10:16:10 -0700

    Update Best Time to Buy and Sell Stock IV.py

--- Best Time to Buy and Sell Stock IV.py
+++ Best Time to Buy and Sell Stock IV.py
@@ -1,5 +1,23 @@
 #Based on the logic from https://www.youtube.com/watch?v=oDhu5uGq_ic
 
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+
+        profit = [[0 for i in range(len(prices))] for j in range(k+1)]
+
+        for i in range(1, k+1):
+            for j in range(1, len(prices)):
+                maxprofit = 0
+                for m in range(0, j):
+                    maxprofit = max(maxprofit, prices[j]-prices[m] + profit[i-1][m])
+                profit[i][j]=max(profit[i][j-1], maxprofit)
+
+        maxprofit = 0
+        for i in range(k+1):
+            maxprofit = max(maxprofit, profit[i][len(prices)-1])
+        return maxprofit
+        
+        
 class Solution(object):
     def maxProfit(self, k, prices):
         """
