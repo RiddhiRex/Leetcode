f81538a2c2c894ccb03f2095a399bf66ab74599d
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-04-05 15:42:44 -0700

    Update Course Schedule.py

--- Course Schedule.py
+++ Course Schedule.py
@@ -1,5 +1,38 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        def dfs(course):
+            if course in seen:
+                return True
+            if course in cycle:
+                return False
+            cycle.add(course)
+            for c in dict[course]:
+                if c not in seen:
+                    if dfs(c)==False:
+                        return False
+            cycle.remove(course)
+            seen.add(course)
+            return True
+        
+        seen = set()
+        cycle = set()
+        dict = collections.defaultdict(list)
+        for p in prerequisites:
+            dict[p[0]].append(p[1])
+        
+        for i in range(numCourses):
+            if i not in seen:
+                if dfs(i)==False:
+                    return False
+        return True
+        
+        
+
+
+#Solution 2:
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def dfs(node, path, pre_req):
             if node in path:
                 return False
@@ -17,10 +50,7 @@ class Solution:
             path.remove(node)
             return True
             
-            
         
-        
-        #pre_req = collections.defaultdict(list)
         pre_req = {}
         for a, b in prerequisites:
             if a not in pre_req.keys():
